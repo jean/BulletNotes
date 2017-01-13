8433ef0d20cfead25c2409b42634ff4f95e7aa09
Nick Busey <nickabusey@gmail.com>
2017-01-12 21:44:11 -0700

    Fixing search

--- imports/ui/pages/search/search.js
+++ imports/ui/pages/search/search.js
@@ -1,11 +1,7 @@
 import './search.jade';
 
-Template.App_search.onCreated(function () {
-  Session.set('searchTerm',FlowRouter.getParam('searchTerm'));
-});
-
 Template.App_search.helpers({
     searchTerm: function () {
-        return Session.get('searchTerm');
+        return FlowRouter.getParam('searchTerm');
     }
 });
