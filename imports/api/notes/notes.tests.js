b74d1aabe79e054946d168c3874594bad681cd1e
Nick Busey <nickabusey@gmail.com>
2017-01-23 18:53:48 -0700

    Setting tab in the newnote line to work as expected.

--- imports/api/notes/notes.tests.js
+++ imports/api/notes/notes.tests.js
@@ -1,11 +1,41 @@
 /* eslint-env mocha */
  
 import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai';
+ 
+import { Notes } from './notes.coffee';
+import './methods.coffee';
  
 if (Meteor.isServer) {
-  describe('Tasks', () => {
+  describe('Notes', () => {
     describe('methods', () => {
-      it('can delete owned task', () => {
+      const userId = Random.id();
+      let noteId;
+ 
+      beforeEach(() => {
+        Notes.remove({});
+        noteId = Notes.insert({
+          text: 'test note',
+          createdAt: new Date(),
+          owner: userId,
+          username: 'tmeasday',
+        });
+      });
+ 
+      it('can delete owned note', () => {
+        // Find the internal implementation of the note method so we can
+        // test it in isolation
+        const deleteNote = Meteor.server.method_handlers['notes.remove'];
+ 
+        // Set up a fake method invocation that looks like what the method expects
+        const invocation = { userId };
+ 
+        // Run the method with `this` set to the fake invocation
+        deleteNote.apply(invocation, [noteId]);
+ 
+        // Verify that the method does what we expected
+        assert.equal(Notes.find().count(), 0);
       });
     });
   });
